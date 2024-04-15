acaf2e69324353e17054162785dafd5d73617384
Gabop <Gabop@gabriel>
2024-04-15 20:45:12 +0200

    second commit

--- src/programacion/fase2/ProgramacionFase2.java
+++ src/programacion/fase2/ProgramacionFase2.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package programacion.fase2;
+import java.util.Scanner;
 
-/**
- *
- * @author Gabop
- */
-public class ProgramacionFase2 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class ProgramacionFase2{
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        int nota = obtenerNota(scanner);
+        verificarNota(nota);
+        
+        scanner.close();
     }
     
+    public static int obtenerNota(Scanner scanner) {
+        System.out.print("Ingrese la nota del alumno: ");
+        return scanner.nextInt();
+    }
+    
+    public static void verificarNota(int nota) {
+        if (nota < 4) {
+            System.out.println("Desaprobado");
+        } else if (nota >= 4 && nota < 10) {
+            System.out.println("Aprobado");
+        } else if (nota == 10) {
+            System.out.println("Sobresaliente");
+        } else {
+            System.out.println("La nota ingresada no es válida");
+        }
+    }
 }
